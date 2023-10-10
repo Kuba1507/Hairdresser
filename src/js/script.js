7a153f792a64f8a2675f5f52c1e0ce60f38ec4df
Kuba1507 <kuba.romanski@onet.com.pl>
2023-10-10 07:42:55 +0200

    styled header for desktop and added typing effect

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,26 @@
 const mobileNav = document.querySelector(".nav-mobile__items");
 const burgerBtn = document.querySelector(".hamburger");
 
+const addTypingEffect = () => {
+	const screenWidth = window.innerWidth;
+	const typingSpan = document.querySelector(".auto-type");
+
+	if (screenWidth >= 992) {
+		typingSpan.classList.add("auto-type");
+		const typed = new Typed(".auto-type", {
+			strings: ["niepowtarzalny", "niezwykły", "osobliwy", "oryginalny"],
+			startDelay: 3000,
+			typeSpeed: 150,
+			backSpeed: 150,
+			loop: true,
+		});
+	}
+};
+
 const handleNav = () => {
 	mobileNav.classList.toggle("show-nav");
 	burgerBtn.classList.toggle("is-active");
 };
 
 burgerBtn.addEventListener("click", handleNav);
+document.addEventListener("DOMContentLoaded", addTypingEffect);
